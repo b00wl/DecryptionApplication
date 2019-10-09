292af0742d82973cfe4b2bf0c2f2d12555702e9b
bowl <grantbrazil@live.com>
2019-10-08 19:41:56 -0500

    add huffman algorithm

--- decryption.py
+++ decryption.py
@@ -1,12 +1,13 @@
+from huffman import HuffmanCoding
+
 
 class Decrypter:
-
     __cert_contents: str
     __decryption_algorithm = ""
 
     def __init__(self, cert_file):
         self.cert_file = cert_file
-        
+
     def __read_cert(self):
         with open(self.cert_file) as cert:
             self.__cert_contents = cert.read()
@@ -14,12 +15,17 @@ class Decrypter:
     def decrypt(self, text):
         self.__read_cert()
         return self.__cert_contents
-        
+
 
 def main():
     decrypter = Decrypter("lib/key.pem")
     out = decrypter.decrypt("test")
-    print(out)
+    # print(out)
+
+    huffman_coding = HuffmanCoding("lib/sample.txt")
+
+    output_path = huffman_coding.compress()
+    huffman_coding.decompress(output_path)
 
 
 if __name__ == "__main__":
