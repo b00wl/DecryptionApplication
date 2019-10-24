015cfdecf266bb74ac796c9cc2872bbe4a346e28
bowl <grantbrazil@live.com>
2019-10-24 18:26:53 -0500

    add argparse

--- decryption.py
+++ decryption.py
@@ -1,32 +1,57 @@
 from huffman import HuffmanCoding
+import argparse
 
 
 class Decrypter:
-    __cert_contents: str
     __decryption_algorithm = ""
 
-    def __init__(self, cert_file):
-        self.cert_file = cert_file
+    def __init__(self, key_file, code_book, file):
+        self.key_file = key_file
+        self.code_book = code_book
+        self.file = file
 
-    def __read_cert(self):
-        with open(self.cert_file) as cert:
-            self.__cert_contents = cert.read()
+    @staticmethod
+    def __read_file(file) -> str:
+        with open(file) as f:
+            __contents = f.read()
+        return __contents
 
-    def decrypt(self, text):
-        self.__read_cert()
-        return self.__cert_contents
+    def decrypt(self):
+        key = self.__read_file(self.key_file)
+        code_book = self.__read_file(self.code_book)
+        file = self.__read_file(self.file)
+
+        huffman_coding = HuffmanCoding("lib/sample.txt")
+        output_path = huffman_coding.compress()
+        huffman_coding.decompress(output_path)
+
+
+def parse_args():
+    """Parse the argument files passed in. Expects fileA, and fileB.
+    Returns:
+        list: The 2 argument files in args.fileA and args.fileB
+    """
+    parser = argparse.ArgumentParser(description='Determine if file is malicious')
+    parser.add_argument('--key', default='',
+                        help="Pass key file name within directory path")
+    parser.add_argument('--codebook', default='',
+                        help="Pass codebook file name within directory path")
+
+    parser.add_argument('--file', default='',
+                        help="Pass file name within directory path")
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    decrypter = Decrypter("lib/key.pem")
-    out = decrypter.decrypt("test")
+    args = parse_args()
+    decrypter = Decrypter(args.key, args.codebook, args.file)
+    decrypter.decrypt()
     # print(out)
-
-    huffman_coding = HuffmanCoding("lib/sample.txt")
-
-    output_path = huffman_coding.compress()
-    huffman_coding.decompress(output_path)
 
 
 if __name__ == "__main__":
     main()
+
+
